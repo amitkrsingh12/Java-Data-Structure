8f953a113b7880f79a8a35d97261f7062024dfca
amitkrsingh12 <severusamit1212@gmail.com>
2025-03-27 21:57:34 +0530

    This code adds two numbers and prints it

---
+++ src/Flowcharts/Sumof2Numbers.java
@@ -0,0 +1,15 @@
+package Flowcharts;
+import java.util.*;
+public class Sumof2Numbers {
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter two numbers :  ");
+        a=in.nextInt();
+        b=in.nextInt();
+        int c= a+b;
+        System.out.println("Sum of two numbers : "+c);
+
+    }
+}
