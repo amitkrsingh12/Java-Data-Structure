f4178436fa2044b8279c35120c311025fccff79b
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-04 22:07:01 +0530

    Check among 3 numbers which is largest using else if

--- src/ConditionalStatements/Largestof3Numbers.java
+++ src/ConditionalStatements/Largestof3Numbers.java
@@ -1,4 +1,21 @@
 package ConditionalStatements;
 
-public class Largestof3Numbers {
+import java.util.Scanner;
+
+public class Largestof3Numbers
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int a=1,b=3,c=6;
+        if(a>b && a>c)
+        {
+            System.out.println("A is largest");
+        }else if(b>c &&b>a)
+        {
+            System.out.println("B is largest");
+        }else{
+            System.out.println("C is largest");
+        }
+    }
 }
