aef398e2f824c80579687ae4cd22114e6e91cfdf
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-04 22:06:20 +0530

    Calculate tax based upon salary

--- src/ConditionalStatements/IncomeTaxCalculator.java
+++ src/ConditionalStatements/IncomeTaxCalculator.java
@@ -1,4 +1,31 @@
 package ConditionalStatements;
-
-public class IncomeTaxCalculator {
+import java.util.*;
+/*
+Income Tax Calculator
+    income < 5L
+    0% tax
+    income between 5-10L
+    20% tax
+    income > 10L
+    30% tax
+ */
+public class IncomeTaxCalculator
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter salary ?");
+        double income= in.nextDouble();
+        double tax;
+        if(income<500000)
+        {
+           tax=0;
+        }else if(income>=500000 && income<1000000)
+        {
+            tax =0.2* income;
+        }else{
+            tax = income*0.3;
+        }
+        System.out.println("Your tax is : " +tax);
+    }
 }
