659a4b7606c26b65fb7b96758aaffa977388e598
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-04 22:04:52 +0530

    Check which number is largest among 2

---
+++ src/ConditionalStatements/Largestof2Number.java
@@ -0,0 +1,19 @@
+package ConditionalStatements;
+/*
+If Else
+ */
+public class Largestof2Number
+{
+    public static void main(String args[])
+    {
+        // Check largest of 2 numbers
+        int a=20,b=5;
+        if(a>=b)
+        {
+            System.out.println("A is largest");
+        }
+        else{
+            System.out.println("B is largest");
+        }
+    }
+}
