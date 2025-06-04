650b4ef2d9537bf4812f2eaead251a166f5ce930
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-04 22:05:26 +0530

    Calculator

--- src/ConditionalStatements/Calculator.java
+++ src/ConditionalStatements/Calculator.java
@@ -1,4 +1,34 @@
 package ConditionalStatements;
 
-public class Calculator {
+import java.util.Scanner;
+
+public class Calculator
+{
+    public static void main(String args[])
+    {
+        Scanner in =new Scanner(System.in);
+        System.out.println("Enter two integer values ?");
+        int a= in.nextInt(), b=in.nextInt();
+        System.out.println("Click the sign you want to perform ?");
+        char ch =in.next().charAt(0);
+        switch(ch){
+        case '+' :
+            System.out.println(a+b);
+            break;
+            case '-':
+                System.out.println(a-b);
+                break;
+            case '*':
+                System.out.println(a*b);
+                break;
+            case '/':
+                System.out.println(a/b);
+                break;
+            default:
+                System.out.println("Wrong key");
+    }
+
+
+
+    }
 }
