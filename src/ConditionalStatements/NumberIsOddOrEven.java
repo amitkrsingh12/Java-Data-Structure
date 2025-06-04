5b1aff8e6a33a4e3f24aaf68d3d6cdf77a6e006c
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-04 22:07:30 +0530

    Number is odd or even

--- src/ConditionalStatements/NumberIsOddOrEven.java
+++ src/ConditionalStatements/NumberIsOddOrEven.java
@@ -1,4 +1,22 @@
 package ConditionalStatements;
 
-public class NumberIsOddOrEven {
+import java.util.Scanner;
+
+/*
+If Else
+ */
+public class NumberIsOddOrEven
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number to check if it is even or odd ?");
+        int n=in.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("Even Number");
+        }else{
+            System.out.println("Odd Number");
+        }
+    }
 }
