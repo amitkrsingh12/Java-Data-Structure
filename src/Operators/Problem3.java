34e2d5b7b00c5376ac51c37c25d9bad152c5bff9
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-06 16:22:46 +0530

    Problem

--- src/Operators/Problem3.java
+++ src/Operators/Problem3.java
@@ -1,4 +1,14 @@
 package Operators;
 
-public class Problem3 {
+public class Problem3
+{
+    public static void main(String args[])
+    {
+        int x,y,z;
+        x=y=z=2;
+        x+=y;
+        y-=z;
+        z/=(x+y);
+        System.out.println(x+ " " + y + " " +z);
+    }
 }
