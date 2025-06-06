c0df6633ca6c698337ff12687cfd1c22d9e50f36
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-06 16:26:31 +0530

    Reverse of given number

--- src/Loops/ReverseNumber.java
+++ src/Loops/ReverseNumber.java
@@ -1,4 +1,22 @@
 package Loops;
 
-public class ReverseNumber {
+import java.util.Scanner;
+
+public class ReverseNumber
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int num;
+        System.out.println("Enter a number ?");
+        num=in.nextInt();
+        int rev=0;
+        while(num!=0)
+        {
+            int digit =num%10;
+            rev =rev*10 +digit;
+            num/=10;
+        }
+        System.out.println(rev);
+    }
 }
