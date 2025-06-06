53db20312233627d5b264b6ba2b731cb6228e6ce
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-06 15:53:04 +0530

    Check if a number is prime or not. Hint : Use of Boolean

--- src/Loops/PrimeNumber.java
+++ src/Loops/PrimeNumber.java
@@ -1,4 +1,33 @@
 package Loops;
 
-public class PrimeNumber {
+import java.util.Scanner;
+
+public class PrimeNumber
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int n;
+        boolean isPrime=true;
+        System.out.println("Enter a number?");
+        n=in.nextInt();
+        if(n==2|| n==0 || n==1){
+            System.out.println("Not Prime");
+        }else{
+            for(int i=2;i<=n/2;i++){
+                if(n%i==0)
+                {
+                    isPrime=false;
+                }
+            }
+            if(isPrime){
+                System.out.println("Prime Number");
+            }else{
+                System.out.println("Not a Prime Number");
+            }
+        }
+
+
+
+    }
 }
