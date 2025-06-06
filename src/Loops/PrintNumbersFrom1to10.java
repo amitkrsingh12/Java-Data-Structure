d93c8b3412d87c09c412f7519aac4aede03471d7
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-06 16:25:33 +0530

    Print

--- src/Loops/PrintNumbersFrom1to10.java
+++ src/Loops/PrintNumbersFrom1to10.java
@@ -1,4 +1,14 @@
 package Loops;
 
 public class PrintNumbersFrom1to10 {
+    public static void main(String args[])
+    {
+        int n=10;
+        int i=1;
+        while(i<=10)
+        {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
 }
