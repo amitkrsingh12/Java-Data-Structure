9eb5f9909b7c71badfd015ac554b80c77bd971b9
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-06 15:08:57 +0530

    Implement Square Pattern

--- src/Loops/PrintSquarePattern.java
+++ src/Loops/PrintSquarePattern.java
@@ -1,4 +1,12 @@
 package Loops;
-
+import java.util.*;
 public class PrintSquarePattern {
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        for(int i=1;i<=4;i++)
+        {
+            System.out.println("****");
+        }
+    }
 }
