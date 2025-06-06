e3c14833cacf66a2d25acb18cab32b0dec41260d
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-06 16:23:46 +0530

    Sum of First N Natural Number using user input

--- src/Loops/SumOfFirstNNaturalNumbers.java
+++ src/Loops/SumOfFirstNNaturalNumbers.java
@@ -1,4 +1,21 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class SumOfFirstNNaturalNumbers {
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter n natural number whose sum you want?");
+        int n=in.nextInt();
+        int sum=0;
+        int counter=1;
+        while(counter<=n)
+        {
+            sum+=counter;
+            counter++;
+            //System.out.println(sum);
+        }
+        System.out.println("Sum of first "+ n +"natural number is  : " +sum);
+    }
 }
