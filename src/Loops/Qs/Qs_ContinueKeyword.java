1eb2c55a9a95cca7982212b429f90c44f7f5c777
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-06 16:26:49 +0530

    Updation

--- src/Loops/Qs/Qs_ContinueKeyword.java
+++ src/Loops/Qs/Qs_ContinueKeyword.java
@@ -1,4 +1,22 @@
 package Loops.Qs;
 
-public class ContinueKeyword {
+import java.util.Scanner;
+/*
+    Display all numbers entered by the user except multiples of 10.
+ */
+public class Qs_ContinueKeyword
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        do{
+            System.out.println("Enter a number");
+           int n=in.nextInt();
+           if(n%10==0)
+           {
+               continue;
+           }
+            System.out.println("Number is  " +n);
+        }while(true);
+    }
 }
