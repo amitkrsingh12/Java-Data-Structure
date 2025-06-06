8d97736bb74f33e7d35def741dbbb91a0693cea8
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-06 15:09:29 +0530

    Print numbers from 1 to N using while loop

--- src/Loops/NumbersFrom1toN.java
+++ src/Loops/NumbersFrom1toN.java
@@ -1,4 +1,18 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class NumbersFrom1toN {
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int i=1,n;
+        System.out.println("Enter N number ?");
+        n=in.nextInt();
+        while(i<=n)
+        {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
 }
