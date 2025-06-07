f23f88036a644508da553173a3d8f06d2a0e3326
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-07 13:49:31 +0530

    Use of Integer.parseInt(args[])

---
+++ src/PriyaranjanSir/a12_CommandLine.java
@@ -0,0 +1,14 @@
+package PriyaranjanSir;
+
+public class a12_CommandLine
+{
+    public static void main(String args[]){
+        int a,b,c;
+        a=Integer.parseInt(args[0]);
+        b=Integer.parseInt(args[1]);
+        System.out.println("1st Value   : " +a);
+        System.out.println("2nd Value   : " +b);
+        c=a+b;
+        System.out.println("Sum         : "  +c);
+    }
+}
