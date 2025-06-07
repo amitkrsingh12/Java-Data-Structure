b8dec6b47a28d489f4cfe3dd60b6a4a59dc78b91
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-07 13:54:09 +0530

    Practical Example of Logical Operator

---
+++ src/PriyaranjanSir/a14_LogicalOperator.java
@@ -0,0 +1,17 @@
+package PriyaranjanSir;
+
+public class a14_LogicalOperator
+{
+    public static void main(String args[])
+    {
+        boolean a=true,b=false;
+        System.out.println("Value of a          " +a);
+        System.out.println("Value of b         " +b);
+        System.out.println("Value of a&&a      " +(a&&a));
+        System.out.println("Value of a&&b      " +(a&&b));
+        System.out.println("Value of a||b      " +(a||b));
+        System.out.println("Value of b||b      " +(b||b));
+        System.out.println("Value of !a        " +(!a));
+        System.out.println("Value of !b        " +(!b));
+    }
+}
