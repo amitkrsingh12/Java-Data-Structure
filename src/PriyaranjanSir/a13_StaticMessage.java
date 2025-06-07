aafad6a991952bd21da8f5501b528a5e6322c808
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-07 13:46:17 +0530

    Static Block Messages comes first in the terminal or output

---
+++ src/PriyaranjanSir/a13_StaticMessage.java
@@ -0,0 +1,12 @@
+package PriyaranjanSir;
+
+public class a13_StaticMessage
+{
+    public static void main(String args[])
+    {
+        System.out.println("Message from main Block.");
+    }
+    static{
+        System.out.println("Message from static block");
+    }
+}
