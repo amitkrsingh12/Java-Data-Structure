33e654841b47456220bd75cf7ac890f2998a5c02
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-07 14:00:26 +0530

    Relational Operator

---
+++ src/PriyaranjanSir/a15_RelationalOperator.java
@@ -0,0 +1,16 @@
+package PriyaranjanSir;
+/*
+Relational or Comparision Operator
+
+ */
+public class a15_RelationalOperator {
+    public static void main(String args[]){
+        int a=10,b=15;
+        System.out.println("Value of a==b   : " +(a==b));
+        System.out.println("Value of a<=b   : " +(a<=b));
+        System.out.println("Value of a>=b   : " +(a>=b));
+        System.out.println("Value of a>b   : " +(a>b));
+        System.out.println("Value of a<b   : " +(a<b));
+        System.out.println("Value of a!=b   : " +(a!=b));
+    }
+}
