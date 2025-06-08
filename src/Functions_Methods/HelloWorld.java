f5d2e93cd6eb22ef8bd83a4bfa672696dc7325b3
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-08 13:24:08 +0530

    Function Example

--- src/Functions_Methods/HelloWorld.java
+++ src/Functions_Methods/HelloWorld.java
@@ -1,4 +1,12 @@
 package Functions_Methods;
 
 public class HelloWorld {
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+        return;
+    }
+    public static void main(String args[])
+    {
+        printHelloWorld();  //function call
+    }
 }
