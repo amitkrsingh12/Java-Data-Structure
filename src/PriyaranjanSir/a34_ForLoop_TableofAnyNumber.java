55c51989c04049f5d30a106590c8490f8f731c23
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-15 11:39:51 +0530

    Multiplication Table

--- src/PriyaranjanSir/a34_ForLoop_TableofAnyNumber.java
+++ src/PriyaranjanSir/a34_ForLoop_TableofAnyNumber.java
@@ -1,4 +1,17 @@
 package PriyaranjanSir;
 
-public class a34_ForLoop_TableofAnyNumber {
+import java.util.Scanner;
+
+public class a34_ForLoop_TableofAnyNumber
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number whose table you want?");
+        int n=in.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n + " * " + i + " = " + (n*i));
+        }
+    }
 }
