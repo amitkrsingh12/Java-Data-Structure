dd5332b049ceb81a1251b05331d43f14566bb30e
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-15 11:40:29 +0530

    Number that is divisible by 1 or itself is called prime number

--- src/PriyaranjanSir/a35_ForLoop_PrimeNumberOrNot.java
+++ src/PriyaranjanSir/a35_ForLoop_PrimeNumberOrNot.java
@@ -1,4 +1,27 @@
 package PriyaranjanSir;
 
-public class a35_ForLoop_PrimeNumberOrNot {
+import java.util.Scanner;
+
+public class a35_ForLoop_PrimeNumberOrNot
+{
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n,i,c=0;
+        System.out.println("Enter any number?");
+        n=in.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                c++;
+            }
+        }
+        if(c==2)
+        {
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("Not Prime");
+        }
+    }
 }
