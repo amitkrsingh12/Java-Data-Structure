d1b49c7d37a0c9e7c496cdbdd1c1b63b865edae0
amitkrsingh12 <severusamit1212@gmail.com>
2025-06-15 11:31:57 +0530

    For Loop Example

---
+++ src/PriyaranjanSir/a32_ForLoop.java
@@ -0,0 +1,16 @@
+package PriyaranjanSir;
+
+import java.util.Scanner;
+
+public class a32_ForLoop
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int i,j;
+        for(i=1,j=20;i<=10;i++,j--)
+        {
+            System.out.println("Value of i = "+ i + " Value of J = " +j);
+        }
+    }
+}
