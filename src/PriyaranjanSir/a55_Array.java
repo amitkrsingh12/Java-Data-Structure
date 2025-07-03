cf13b61c8890ce552dd17f2c5506c7e3f0568e37
amitkrsingh12 <severusamit1212@gmail.com>
2025-07-03 22:58:43 +0530

    Array

--- src/PriyaranjanSir/a55_Array.java
+++ src/PriyaranjanSir/a55_Array.java
@@ -1,4 +1,15 @@
 package PriyaranjanSir;
 
-public class a55_Array {
+import java.util.Scanner;
+
+public class a55_Array
+{
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int i,arr[]={34,45,67,89,12,34,88,56,89,34,78};
+        for(i=0;i<10;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
 }
