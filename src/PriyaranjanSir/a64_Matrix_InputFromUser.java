7ac6dce423932ba6700985d91162d76444749caa
amitkrsingh12 <severusamit1212@gmail.com>
2025-07-04 16:28:26 +0530

    Matrix Input from user

--- src/PriyaranjanSir/a64_Matrix_InputFromUser.java
+++ src/PriyaranjanSir/a64_Matrix_InputFromUser.java
@@ -1,4 +1,30 @@
 package PriyaranjanSir;
 
-public class a64_Matrix_InputFromUser {
+import java.util.Scanner;
+
+public class a64_Matrix_InputFromUser
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int i,j,arr[][] = new int [3][3];
+        System.out.println("Enter matrix element?");
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                System.out.print("arr[" + i + "] [" + j + "]=");
+                arr[i][j]=in.nextInt();
+            }
+        }
+        System.out.println("Matrix element : ");
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                System.out.print(arr[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
 }
